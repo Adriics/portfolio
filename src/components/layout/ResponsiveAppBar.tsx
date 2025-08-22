1a77e329f583f4ca02107c1f0d4a0c675efee113
Adriics <adriancalvachesanchez@gmail.com>
2025-08-22 15:26:44 +0200

    feat: update responsive app bar, and finished

--- src/components/layout/ResponsiveAppBar.tsx
+++ src/components/layout/ResponsiveAppBar.tsx
@@ -95,7 +95,6 @@ function ResponsiveAppBar() {
               ))}
             </Menu>
           </Box>
-          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
           <Typography
             variant="h5"
             noWrap
@@ -112,7 +111,7 @@ function ResponsiveAppBar() {
               textDecoration: "none",
             }}
           >
-            LOGO
+            AdriCS
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
